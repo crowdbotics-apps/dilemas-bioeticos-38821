772d53809088fbaf062e13bba78b084dc27ddd22
Crowdbotics <team@crowdbotics.com>
2023-02-09 00:30:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dilemas_bioeticos_38821/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dilemas_bioeticos_38821;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "dilemas_bioeticos_38821";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dilemas-bioeticos-38821",
+  url: "https://dilemas-bioeticos-38821.botics.co",
+  api: "https://dilemas-bioeticos-38821.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
